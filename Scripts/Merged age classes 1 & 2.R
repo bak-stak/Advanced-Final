70baab9c3d61631cb28e550dc9e17caaf6e39fe1
bak-stak <giobak33@gmail.com>
2026-06-19 11:39:45 +0200

    Merged correction

--- Scripts/Merged age classes 1 & 2.R
+++ Scripts/Merged age classes 1 & 2.R
@@ -44,13 +44,12 @@ lapply(pca_library, library, character.only = TRUE)
 #Keep just relevant VARs
 
 trust_data_NA <- ITANES_2022_Post_electoral%>%
-  select(seriale, Q04b, Q05b, Q05c, Q07a, Q07b, Q07e, Q100a, Q12, Q15, Q22)%>%
+  select(seriale, Q04b, Q05b, Q05c, Q07a, Q07b, Q07e)%>%
   mutate(across(-seriale, ~ ifelse(.x >= 50, NA, .x))) %>%
   mutate( #reverse were needed to put the negative connotation on the high end
     Q04b = 6 - Q04b,
     Q05b = 6 - Q05b,
     Q05c = 6 - Q05c,
-    Q15  = 6 - Q15
   )
 
 # NA imputation
@@ -74,19 +73,14 @@ trust_data <- complete(imputation, 1)
 
 #Transform Q06 as numeric
 
-# Isolate cateogrical variable (every variable except for q06 which is a 0-10 likert scale)
-# For Fa is key not to mix continuos and categorical var and advise the model when you do it
-# For this reason i am creating cat_var
+# Create a var vector for convenience
 
-cat_var <- c("Q04b", "Q05b", "Q05c", "Q07a", "Q07b","Q07e", 
-             "Q12", "Q15", "Q22") 
-
-cat_var2 <- c("Q04b", "Q05b", "Q05c", "Q07a", "Q07b","Q07e") 
+cat_var <- c("Q04b", "Q05b", "Q05c", "Q07a", "Q07b","Q07e") 
 
 # With mixcor we impute a mixed correlation matrix
 
 mixed_cormat <- mixedCor(data = trust_data,
-                         c = cat_var2,
+                         c = cat_var,
                          smooth = TRUE)
 
 # Plot the correlation 
@@ -189,10 +183,10 @@ trust_rescaled <- trust_data %>%
 populist_df <- ITANES_2022_Post_electoral%>%
   select(seriale, eta_classe, Q10LHa, Q10LHb,eta)%>%
   mutate(seriale = as.numeric(zap_labels(seriale)),
-         Abstension = as.numeric(zap_labels(Q10LHa)))%>%
-  filter(Abstension < 3)%>%
-  mutate(Abstension = ifelse(Abstension == 2, 1, 0),
-         Abstension = as.factor(Abstension))%>%
+         abstention = as.numeric(zap_labels(Q10LHa)))%>%
+  filter(abstention < 3)%>%
+  mutate(abstention = ifelse(abstention == 2, 1, 0),
+         abstention = as.factor(abstention))%>%
   mutate(populist_vote = Q10LHb %in% c("2","5"))%>%
   mutate(eta_classe = fct_collapse( #merging classes 1 & 2
     factor(eta_classe),
@@ -203,7 +197,7 @@ populist_df <- ITANES_2022_Post_electoral%>%
     ">64"   = "6"
   ))%>%
   mutate(age_class = eta_classe)%>%
-  select(seriale, age_class, populist_vote, eta, Abstension)
+  select(seriale, age_class, populist_vote, eta, abstention)
 
 
 pop_age <- left_join(trust_rescaled, populist_df, by = join_by(seriale))
@@ -229,37 +223,73 @@ plot_model(Complete_pooling,
 
 #Try it on vote/non vote
 
-CP_abstension <- glm(Abstension ~ Institutional_distrust,
+CP_abstention <- glm(abstention ~ Institutional_distrust,
                      family = binomial(link = "logit"),
                      data = pop_age)
 
-stargazer(CP_abstension, type = "text")
+stargazer(CP_abstention, type = "text")
 
-plot_model(CP_abstension, 
+plot_model(CP_abstention, 
            type = "pred", 
            terms = "Institutional_distrust",
 ) +
   theme_minimal()
 
-#try fitting the model only on voters (exclude abstension)
+# GAM - Check the shape of the distribution ----
 
-no_abs <- pop_age%>%
-  mutate(Abstension = as.numeric(Abstension))%>%
-  filter(Abstension<2)
+# A gam is a model that guarantee a high flexibility & calculate non linear regression models
+# we want to use it to study the shape of the relation
 
-CP_voters <- glm(populist_vote ~ Institutional_distrust,
-                 family = binomial(link = "logit"),
-                 data = no_abs)
+library(mgcv)
 
-stargazer(CP_voters, type = "text")
+mod_gam <- gam(populist_vote
+               ~ s(Institutional_distrust)
+               + age_class,
+               family = binomial, data = pop_age_clean)
 
-plot_model(CP_voters, 
+plot(mod_gam, pages = 1, shade = TRUE)
+
+summary(mod_gam)
+
+# It seems like at a certain point the populist vote decrease for institutional distrust
+# This can suggest that after a certain ceiling non voting become the choice: this could
+# be the reason for our non working models. We can try a model that take institutional
+# distrust till 7/8 and check the significativity of the relation and we can do the same
+# for non vote. Moreover we can impute a GAM for abstention
+
+
+mod_gam <- gam(abstention
+               ~ s(Institutional_distrust)
+               + age_class,
+               family = binomial, data = pop_age)
+
+plot(mod_gam, pages = 1, shade = TRUE)
+
+# Our HP was right abstention explode for really high levels of distrust 
+# Now we can try our CDT (critical distrust threshold) model with level lower than 8
+
+# Before starting with our plan we want to check how many high distrust individuals we have
+table(pop_age$Institutional_distrust >= 8)
+
+# 987 is really significant, this means that they have a strong impact on our regression models
+
+# CDT simple model ----
+
+CDT_df <- pop_age_clean%>%
+  filter(Institutional_distrust <= 8)
+
+CDT <- glm(populist_vote ~ Institutional_distrust,
+                        family = binomial(link = "logit"),
+                        data = CDT_df)
+
+stargazer(CDT, type = "text")
+
+plot_model(CDT, 
            type = "pred", 
-           terms = "Institutional_distrust",
+           terms = "Institutional_distrust [all]",
+           
 ) +
   theme_minimal()
-
-
 
 # INTERACTION MODEL ----
 
@@ -301,7 +331,7 @@ sjPlot::plot_model(distrust_x_class,
 ) +
   theme_bw()
 
-distrust_x_class_abs <- glm(Abstension
+distrust_x_class_abs <- glm(abstention
                             ~ Institutional_distrust
                             * age_class,
                             family = binomial(link = "logit"),
@@ -315,49 +345,10 @@ sjPlot::plot_model(distrust_x_class_abs,
 ) +
   theme_bw()
 
-# GAM - Check the shape of the distribution ----
-
-# A gam is a model that guarantee a high flexibility & calculate non linear regression models
-# we want to use it to study the shape of the relation
-
-library(mgcv)
-
-mod_gam <- gam(populist_vote
-               ~ s(Institutional_distrust)
-               + age_class,
-               family = binomial, data = pop_age_clean)
-
-plot(mod_gam, pages = 1, shade = TRUE)
-
-
-# It seems like at a certain point the populist vote decrease for institutional distrust
-# This can suggest that after a certain ceiling non voting become the choice: this could
-# be the reason for our non working models. We can try a model that take institutional
-# distrust till 7/8 and check the significativity of the relation and we can do the same
-# for non vote. Moreover we can impute a GAM for abstension
-
-
-mod_gam <- gam(Abstension
-               ~ s(Institutional_distrust)
-               + age_class,
-               family = binomial, data = pop_age)
-
-plot(mod_gam, pages = 1, shade = TRUE)
-
-# Our HP was right abstension explode for really high levels of distrust 
-# Now we can try our DBNSM (distrust but not so much) model with level lower than 8
-
-# Before starting with our plan we want to check how many high distrust individuals we have
-table(pop_age$Institutional_distrust >= 8)
-
-# 987 is really significant, this means that they have a strong impact on our regression models
 
 # DBNSM Model -----
 
 # DBNSM addictive
-
-DBNSM_df <- pop_age_clean%>%
-  filter(Institutional_distrust <= 8)
 
 DBNSM <- glm(populist_vote
              ~ Institutional_distrust,
@@ -432,10 +423,10 @@ plot_model(Regions_model_add,
 ) +
   theme_minimal()
 
-# Pure addittive model of abstension with control
+# Pure addittive model of abstention with control
 
 
-Regions_model_add_abs <- glm(Abstension
+Regions_model_add_abs <- glm(abstention
                              ~ Macro_region + Institutional_distrust
                              + age_class,
                              family = binomial(link = "logit"),
@@ -467,9 +458,9 @@ plot_model(Regions_model_int,
 ) +
   theme_minimal()
 
-# Interaction model of abstension with control 
+# Interaction model of abstention with control 
 
-Regions_model_int_abs <- glm(Abstension
+Regions_model_int_abs <- glm(abstention
                              ~ Macro_region + Institutional_distrust
                              * age_class,
                              family = binomial(link = "logit"),
@@ -504,13 +495,13 @@ Populist_vote <- modelsummary(
 )
 
 
-Abstension <- modelsummary(
+abstention <- modelsummary(
   list(
     "Interaction model" = distrust_x_class_abs,
     "Region controlling (Additive)" = Regions_model_add_abs,
     "Region controlling (Interaction)" = Regions_model_int_abs
   ),
-  output = "Abstension_regression.docx",
+  output = "abstention_regression.docx",
   stars = TRUE,
   gof_omit = "IC|Log|Adj|Within|Pseudo"
 )
